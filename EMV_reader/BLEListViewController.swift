e719bfd307e49a3cee1d8ff04145dc1c2701a7c7
Piotr Ilski <piotr.ilski@gmail.com>
2018-10-16 14:52:27 +0200

    Derive Key based on KSN and IPEC

--- EMV_reader/BLEListViewController.swift
+++ EMV_reader/BLEListViewController.swift
@@ -146,7 +146,89 @@ class Decrypto {
         return bytes
     }
     
+    /**
+     * Get the counter bits from your original (not masked!) 10-byte KSN
+     * by ANDing its bottom three bytes with 0x1FFFFF.
+     * (Recall that the bottom 21 bits of a KSN comprise the transaction counter.)
+     */
+    func getCounterBits(ksnHex: String) -> [UInt8] {
+        let bottomThree = Array(hexToByteArray(hex: ksnHex).suffix(3))
+        
+        return binaryAnd(binaryDataToHexString(bytes: bottomThree), "1FFFFF")
+    }
+    
     func getKey(bdkHex: String, ksnHex: String) -> String {
+        let derivedKey = deriveKey(bdkHex: bdkHex, ksnHex: ksnHex)
+        
+        return derivedKey
+    }
+    
+    func deriveKey(bdkHex: String, ksnHex: String) -> String {
+        let IPEK = getIPEK(bdkHex: bdkHex, ksnHex: ksnHex)
+        
+        let bottomEightFromKSN = Array(hexToByteArray(hex: ksnHex).suffix(8))
+        let baseKSN = binaryAnd("FFFFFFFFFFE00000", binaryDataToHexString(bytes: bottomEightFromKSN))
+        
+        let counter = getCounterBits(ksnHex: ksnHex)
+        var currKey = IPEK
+        
+        print("counter = \(counter)")
+        print("currKey = \(currKey)")
+        
+        let counterInt = Int(binaryDataToHexString(bytes: counter), radix: 16)!
+        var baseKSNInt = Int(binaryDataToHexString(bytes: baseKSN), radix: 16)!
+        
+        print("counterInt = \(counterInt)")
+        print("baseKSN = \(baseKSN)")
+        
+        var shiftReg = 0x100000
+        var pass = 0
+        
+        while(shiftReg > 0) {
+            if ((shiftReg & counterInt) > 0) {
+                baseKSNInt |= shiftReg
+                currKey = binaryDataToHexString(
+                    bytes: generateKey(key: currKey, ksn: String(format:"%llX", baseKSNInt)));
+                
+                let ksnHex = String(format:"%llX", baseKSNInt)
+                let keyHex = currKey
+                
+                print("Pass \(pass), baseKSN:\(ksnHex) key: \(keyHex)")
+                
+                pass += 1
+            }
+            
+            shiftReg >>= 1
+        }
+        
+        return  currKey
+    }
+    
+    func generateKey(key: String, ksn: String) -> [UInt8] {
+        let mask = "C0C0C0C000000000C0C0C0C000000000"
+        let maskedKey =  binaryXOR(mask, key);
+        
+        let left  = encryptRegister(key: maskedKey, ksn: hexToByteArray(hex: ksn));
+        let right = encryptRegister(key: hexToByteArray(hex: key), ksn: hexToByteArray(hex: ksn));
+
+        return left + right
+    }
+    
+    func encryptRegister(key: [UInt8], ksn: [UInt8]) -> [UInt8] {
+        let bottomEight = Array(key.suffix(8))
+        let topEight = Array(key.prefix(8))
+        let initialVector: [UInt8] = hexToByteArray(hex: "0000000000000000")
+        let bottomEightXORKSN = binaryXOR(binaryDataToHexString(bytes: bottomEight),
+                                          binaryDataToHexString(bytes: ksn))
+        let desEncrypted = Array(singleDesEncrypt(data: bottomEightXORKSN,
+                                            keyData: topEight,
+                                            iv: initialVector)!.prefix(8))
+        
+        return binaryXOR(binaryDataToHexString(bytes: bottomEight),
+                         binaryDataToHexString(bytes: desEncrypted))
+    }
+    
+    func getIPEK(bdkHex: String, ksnHex: String) -> String {
         
         let extendedBdk = extendBDK(bdk: bdkHex)
         let maskedKSN = binaryAnd(ksnHex, "FFFFFFFFFFFFFFE00000")
@@ -196,6 +278,40 @@ class Decrypto {
         }
     }
     
+    
+    func singleDesEncrypt(data: [UInt8], keyData: [UInt8], iv: [UInt8]) -> [UInt8]? {
+        let cryptData = NSMutableData(
+            length: Int(data.count) + kCCBlockSizeDES)!
+        let keyLength              = size_t(kCCKeySizeDES)
+        let operation: CCOperation = UInt32(kCCEncrypt)
+        let algoritm:  CCAlgorithm = UInt32(kCCAlgorithmDES)
+        let options:   CCOptions   = UInt32(kCCOptionECBMode + kCCOptionPKCS7Padding)
+        
+        var numBytesEncrypted :size_t = 0
+        
+        let cryptStatus = CCCrypt(operation,
+                                  algoritm,
+                                  options,
+                                  keyData,
+                                  keyLength,
+                                  iv,
+                                  data,
+                                  data.count,
+                                  cryptData.mutableBytes,
+                                  cryptData.length,
+                                  &numBytesEncrypted)
+        
+        
+        if UInt32(cryptStatus) == UInt32(kCCSuccess) {
+            cryptData.length = Int(numBytesEncrypted)
+            
+            return [UInt8](cryptData as Data)
+            
+        } else {
+            print("Error: \(cryptStatus)")
+        }
+        return nil
+    }
     
     func desEncrypt(data: [UInt8], keyData: [UInt8], iv: [UInt8]) -> [UInt8]? {
         let cryptData = NSMutableData(
@@ -292,7 +408,7 @@ class BLEListViewController: UIViewController {
         let decrypto = Decrypto()
         
         let bdk = "0123456789ABCDEFFEDCBA9876543210"
-        let ksn = "62994900000000000001"
+        let ksn = "629949012C0000000003"
         
         let IPEK = decrypto.getKey(bdkHex: bdk, ksnHex: ksn)
         
